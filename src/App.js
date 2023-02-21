3eb972b4fad2d2fd9a2f74d68f54e2e40c373764
deepak <deepak.kondapalli29@gmail.com>
2023-02-21 12:40:59 +0530

    step-3 of checkpoint-1 delta

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from './components/Navbar/Navbar';
+import Banner from './components/Banner/Banner';
 
 function App(){
     return (
         <div className="App">
           <Navbar />
+          <Banner />
         </div>
       );
 }
--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,15 +1,47 @@
 import "./Banner.css";
 import { useState, useEffect } from "react";
-import axios from "axios";
+// import axios from "axios";
 import requests from "../../request";
 
 const Banner = () => {
   const [movie, setMovie] = useState([]);
   useEffect(() => {
-
+        async function fetchData() {
+      const response = await fetch(requests.fetchNetflixOriginals);
+      const data = await response.json();
+      const randomMovie = Math.floor(Math.random() * data.results.length);
+      setMovie(data.results[randomMovie]);
+    }
+    fetchData();
   }, []);
 
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
   return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url(
+          "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
+        )`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <h1 className="banner__description">{truncate(movie?.overview, 150)}</h1>
+      </div>
+      <div className="banner--fadeBottom" />
+    </header>
 
   );
 };
